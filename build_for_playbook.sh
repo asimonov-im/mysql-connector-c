28d4e1087c2b65d071c31e7ec044f49096c0898e
Artem Simonov <asimonov@rim.com>
2012-04-05 17:42:56 -0400

    2012-04-05  Artem Simonov  <asimonov@rim.com>
            Used the porting-utils qcc compilers and called make install on completion.
            Also removed CMakeCache.txt before starting, due to errors.
    
            * PlayBookToolchain.cmake:
            * build_for_playbook.sh:

--- build_for_playbook.sh
+++ build_for_playbook.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm -f CMakeCache.txt
 cmake -G "Unix Makefiles" --debug-trycompile -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DCMAKE_TOOLCHAIN_FILE=PlayBookToolchain.cmake
-make
+make install
+
